6addbef2f44717f1e9914c90d1832968017931f3
Dave Horton <daveh@beachdognet.com>
2020-02-20 07:52:32 -0500

    initial checkin

---
+++ index.js
@@ -0,0 +1,16 @@
+const assert = require('assert');
+
+module.exports = responseTime
+
+const responseTime  = (fn) => (req, res, next) => {
+  var startAt = process.hrtime()
+
+  res.once('end', () => {
+    var diff = process.hrtime(startAt);
+    var time = diff[0] * 1e3 + diff[1] * 1e-6;
+
+    fn(req, res, time);
+  });
+
+  next();
+};
